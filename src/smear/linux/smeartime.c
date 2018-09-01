5be1dbacac04477f2b057640865af92b0029751f
Nathan Michaels <nathan@nmichaels.org>
2018-09-01 19:07:33 -0400

    Slightly cleaner.

--- src/smear/linux/smeartime.c
+++ src/smear/linux/smeartime.c
@@ -1,4 +1,3 @@
-#define _POSIX_C_SOURCE 199309L
 #include <time.h>
 #include "smeartime.h"
 
--- src/smear/module.mk
+++ src/smear/module.mk
@@ -2,4 +2,5 @@ OS ?= linux
 SRC += smear.c
 LIBS += -pthread
 VPATH := $(VPATH) src/smear/$(OS)/
+CFLAGS += -D_POSIX_C_SOURCE=199309L
 SRC += smeartime.c
