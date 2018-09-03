95d21d4185a208865309981515a90823e08c44d6
Nathan Michaels <nathan@nmichaels.org>
2018-09-03 10:34:24 -0400

    Maybe fix windows build?

--- src/smear/Windows_NT/smeartime.c
+++ src/smear/Windows_NT/smeartime.c
@@ -1,13 +1,13 @@
+#include <windows.h>
 #include <Winbase.h>
 #include "smeartime.h"
 
-#define NANOSECONDS_PER_SECOND 1000000000
+#define NANOSECONDS_PER_MILLISECOND 1000
 
 uint64_t get_now_ns(void)
 {
-    struct timespec ts;
     uint64_t now;
 
     now = GetTickCount64();
-    return now * 1000; // Will overflow after 584,542 years of uptime.
+    return now * NANOSECONDS_PER_MILLISECOND;
 }
