56beb30e3aa93b6abad53f9a9ed5726736f884c6
natebragg <nate@cs.tufts.edu>
2018-10-10 09:39:39 -0400

    Release 0.1.1

--- include/smear/version.h
+++ include/smear/version.h
@@ -1,8 +1,9 @@
 #ifndef __SMEAR_VERSION_H__
 #define __SMEAR_VERSION_H__
 
-#define SMEAR_VERSION "0.1"
+#define SMEAR_VERSION "0.1.1"
 #define SMEAR_VERSION_MAJOR 0
 #define SMEAR_VERSION_MINOR 1
+#define SMEAR_VERSION_PATCH 1
 
 #endif
